b67085590e3543e2281d84702414b67aeb61960e
yohansbekele12 <yohansdemisie@gmail.com>
2025-03-04 17:56:42 +0100

    db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,76 @@
+// models/index.js
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { Sequelize } from 'sequelize';
+import config from '../config/config.json' assert { type: 'json' };
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: console.log,
-  }
-);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
 
-export default sequelize;
+const db = {};
+
+let sequelize;
+if (dbConfig.use_env_variable) {
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
+} else {
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    dbConfig
+  );
+}
+
+// Dynamically import all models
+const modelFiles = fs.readdirSync(__dirname)
+  .filter(file => 
+    file.indexOf('.') !== 0 && 
+    file !== basename && 
+    file.slice(-3) === '.js'
+  );
+
+for (const file of modelFiles) {
+  const modelPath = path.join(__dirname, file);
+  const model = (await import(modelPath)).default(sequelize);
+  db[model.name] = model;
+}
+
+// Define associations
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+// Define specific associations
+db.User.hasMany(db.Student, { foreignKey: 'parent_id' });
+db.Student.belongsTo(db.User, { foreignKey: 'parent_id' });
+
+db.Student.belongsToMany(db.Subject, { through: db.StudentSubject });
+db.Subject.belongsToMany(db.Student, { through: db.StudentSubject });
+
+db.Student.hasMany(db.BehaviorRecord);
+db.BehaviorRecord.belongsTo(db.Student);
+
+db.Student.hasMany(db.SkillsGift);
+db.SkillsGift.belongsTo(db.Student);
+
+db.User.hasMany(db.Notification);
+db.Notification.belongsTo(db.User);
+
+db.User.hasMany(db.Flag, { as: 'FlaggedBy', foreignKey: 'flagged_by_user_id' });
+db.User.hasMany(db.Flag, { as: 'FlaggedUser', foreignKey: 'target_user_id' });
+db.Flag.belongsTo(db.User, { as: 'FlaggedBy', foreignKey: 'flagged_by_user_id' });
+db.Flag.belongsTo(db.User, { as: 'FlaggedUser', foreignKey: 'target_user_id' });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
